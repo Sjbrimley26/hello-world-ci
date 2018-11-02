f267d45e1e9dca8ab533a117f3529d09dd64e5fd
Spencer Brimley <Sjbrimley26@live.com>
2018-11-01 17:01:57 -0700

    Initial commit. Just starting with a boilerplate

---
+++ handler.js
@@ -0,0 +1,19 @@
+/*jshint node: true, esversion: 6 */
+'use strict';
+
+const HelloWorld = require('./lib/hello-world');
+
+module.exports.helloWorld = (event, context, callback) => {
+
+  var hWorld = new HelloWorld();
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify(hWorld.sayHello(event)),
+  };
+
+  callback(null, response);
+};
